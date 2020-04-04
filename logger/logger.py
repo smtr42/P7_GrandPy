72bd948706dc0fc31c6508e19caa35b61b883545
smtr42 <simteiva@yahoo.fr>
2020-04-04 19:56:16 +0200

    Added logging capabilities

---
+++ logger/logger.py
@@ -0,0 +1,33 @@
+"""Set up a simple logger for the application"""
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+def setup_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        '%(asctime)s [%(levelname)s] | File \"%(filename)s\" in \"%('
+        'funcName)s\" line %(lineno)s: %(message)s')
+
+    file_handler = RotatingFileHandler('logger/log/activity.log',
+                                       'a',
+                                       100000,
+                                       1
+                                       )
+
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+
+    return logger, stream_handler
+
+
+logger, stream_handler = setup_logger()
