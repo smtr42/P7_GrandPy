0753cb4ab1c723b2230132a759f5011b33e8432a
smtr42 <simteiva@yahoo.fr>
2020-04-04 19:57:07 +0200

    Added a class parser

---
+++ models/parser.py
@@ -0,0 +1,18 @@
+"""
+Parse user input to have exploitable data to request to APIs.
+"""
+from logger.logger import logger
+
+
+class Parser:
+    """ Parse input data."""
+    def __init__(self):
+        pass
+
+    def func1(self):
+        pass
+
+    def func2(self):
+        pass
+
+logger.debug("test")
