3cae5eab03079a0d563dc8a06228b760aa1dcce8
smtr42 <simteiva@yahoo.fr>
2020-04-05 16:36:44 +0200

    added localisation word

--- config/config.py
+++ config/config.py
@@ -1,12 +1,15 @@
+# # Google Maps API basis
+# MAP_API_KEY = os.environ['MAP_API_KEY']
+# MAP_URL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
+# MAP_PAYLOAD = {"input":"", "inputtype":"textquery",
+#     "fields":"formatted_address,name,geometry", "key": MAP_API_KEY}
+#
+# # Wikipedia API basis
+# WIKI_URL = "https://fr.wikipedia.org/w/api.php"
+# WIKI_PAYLOAD = {"action":"query", "list":"search", "srsearch":"",
+#     "utf8":"", "format":"json"}
 
 
-# Google Maps API basis
-MAP_API_KEY = os.environ['MAP_API_KEY']
-MAP_URL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
-MAP_PAYLOAD = {"input":"", "inputtype":"textquery",
-    "fields":"formatted_address,name,geometry", "key": MAP_API_KEY}
-
-# Wikipedia API basis
-WIKI_URL = "https://fr.wikipedia.org/w/api.php"
-WIKI_PAYLOAD = {"action":"query", "list":"search", "srsearch":"",
-    "utf8":"", "format":"json"}
+key_localisation = ['endroit', 'adresse', 'rue', 'lieu', 'place',
+                    'coordonnées', 'localiser', 'où sont', 'où est',
+                    'localisation', ]
