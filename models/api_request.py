286c99f459e812333a1498211466660c2cf578f4
smtr42 <simteiva@yahoo.fr>
2020-04-10 23:22:01 +0200

    added new request api classes skeleton

---
+++ models/api_request.py
@@ -0,0 +1,29 @@
+""" This will handle any API request."""
+
+
+class GoogleRequest:
+    """Will handle the request for Google API."""
+
+    def __init__(self, query):
+        self.url = None
+        self.key = None
+        self.query = query
+        pass
+
+    def api_request(self):
+
+        pass
+
+    def filter(self):
+        pass
+
+
+class GoogleGeoCode:
+    """Format the API answer into useful data."""
+
+    def __init__(self):
+        pass
+
+
+class WikipediaRequest:
+    pass
